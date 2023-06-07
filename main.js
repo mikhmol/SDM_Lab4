95fc942771350b328d1da93b392b87369a84d4d0
mikhmol <mikhailo.moliakov@gmail.com>
2023-06-07 18:32:22 +0300

    Added editing, marking task functions and main code

--- main.js
+++ main.js
@@ -34,6 +34,19 @@ function showAllTasks() {
     });
   }
 
+  function markTaskAsCompleted(taskTitle) {
+    const tasks = loadTasks();
+    const task = tasks.find((task) => task.title === taskTitle);
+    if (task) {
+      task.completed = true;
+      task.completedDate = new Date().toISOString();
+      saveTasks(tasks);
+      console.log(`Task "${taskTitle}" marked as completed.`);
+    } else {
+      console.log(`Task "${taskTitle}" not found.`);
+    }
+  }
+
 function addTask(title, description, deadline) {
   const tasks = loadTasks();
   const task = {
@@ -47,6 +60,30 @@ function addTask(title, description, deadline) {
   console.log(`Task "${title}" added.`);
 }
 
+function editTask(taskTitle, newTitle, newDescription, newDeadline) {
+    const tasks = loadTasks();
+    const task = tasks.find((task) => task.title === taskTitle);
+    if (task) {
+      task.title = newTitle || task.title;
+      task.description = newDescription || task.description;
+      task.deadline = newDeadline || task.deadline;
+      saveTasks(tasks);
+      console.log(`Task "${taskTitle}" edited.`);
+    } else {
+      console.log(`Task "${taskTitle}" not found.`);
+    }
+  }
+
+  function showOverdueTasks() {
+    const tasks = loadTasks();
+    const today = new Date();
+    const overdueTasks = tasks.filter((task) => !task.completed && new Date(task.deadline) < today);
+    console.log('Overdue Tasks:');
+    overdueTasks.forEach((task) => {
+      console.log(`- ${task.title} (Deadline: ${task.deadline})`);
+    });
+  }
+
 function deleteTask(taskTitle) {
     const tasks = loadTasks();
     const index = tasks.findIndex((task) => task.title === taskTitle);
@@ -57,4 +94,23 @@ function deleteTask(taskTitle) {
     } else {
       console.log(`Task "${taskTitle}" not found.`);
     }
+  }
+
+  if (process.argv.length < 3) {
+    console.log('Usage: node app.js <command> [arguments]');
+    process.exit(1);
+  }
+
+  const command = process.argv[2];
+
+  if (command === 'show-incomplete') {
+    showIncompleteTasks();
+  } else if (command === 'show-all') {
+    showAllTasks();
+  } else if (command === 'mark-completed') {
+    const taskTitle = process.argv[3];
+    markTaskAsCompleted(taskTitle);
+  } else {
+    console.log('Invalid command.');
+    process.exit(1);
   }
